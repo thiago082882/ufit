8376fff263a1f5f5f6c8d17a1403988e246d0bc2
thiago082882 <thiagocostadejesus2@gmail.com>
2023-04-29 10:57:37 -0300

    include

---
+++ migrations/migration.sql
@@ -0,0 +1,30 @@
+"use strict";
+
+exports.up = function(next) {
+  const sql = `
+    CREATE TABLE users (
+      id BIGSERIAL PRIMARY KEY,
+      email VARCHAR(255) NOT NULL UNIQUE,
+      name VARCHAR(255) NOT NULL,
+      lastname VARCHAR (255) NOT NULL,
+      gender VARCHAR(100) NOT NULL,
+      birthdate DATE NOT NULL,
+      weight VARCHAR(50) NOT NULL,
+      height VARCHAR(50) NOT NULL,
+      image VARCHAR(255) NULL,
+      password VARCHAR(255) NOT NULL,
+      is_available BOOLEAN NULL,
+      session_token VARCHAR(255) NULL,
+      created_at TIMESTAMP(0) NOT NULL,
+      updated_at TIMESTAMP (0) NOT NULL
+    );
+  `;
+  next(null, sql);
+};
+
+exports.down = function(next) {
+  const sql = `
+    DROP TABLE users;
+  `;
+  next(null, sql);
+};
